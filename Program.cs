c8de14e96ac5af1d7945b20b2125c320bc1b6e55
Yassine-Bouazzaoui <151935523+Yassine-Bouazzaoui@users.noreply.github.com>
2025-03-09 13:24:13 +0000

    improve create and edit offre pages style

--- Program.cs
+++ Program.cs
@@ -7,7 +7,6 @@ using RecrutementApplication.Utility;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
@@ -24,11 +23,9 @@ builder.Services.AddScoped<IEmailSender, EmailSender>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
